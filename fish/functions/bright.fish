458f39a7d663d823923a45d41138f0e07df2fd7b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-30 18:21:51 -0400

    Update bright function

--- fish/functions/bright.fish
+++ fish/functions/bright.fish
@@ -1,11 +1,20 @@
-function bright -a bright -d "Changes brightness on Intel backlight systems"
+function bright -a direction bright -d "Changes brightness on Intel backlight systems"
         set -l max (cat /sys/class/backlight/intel_backlight/max_brightness)
-        set -l min  (math "0.05*$max")
-        if test $bright -gt $max
-                echo "Brightness cannot exceed $max"
-        else if test $bright -le $min
-                echo "I wouldn't do that if I were you..."
-        else
-                echo $bright | sudo tee /sys/class/backlight/intel_backlight/brightness
+        set -l min (math "0.05*$max")
+        set -l step (math "$bright*$max")
+        set -l current (cat /sys/class/backlight/intel_backlight/actual_brightness)
+        switch $direction
+                case 'up'
+                        if test (math "$step+$current") -gt $max
+                                echo $max | tee /sys/class/backlight/intel_backlight/brightness
+                        else
+                                echo (math "$step+$current") | tee /sys/class/backlight/intel_backlight/brightness
+                        end
+                case 'down'
+                        if test (math "$current-$step") -le $min
+                                echo $min | tee /sys/class/backlight/intel_backlight/brightness
+                        else
+                                echo (math "$current-$step") | tee /sys/class/backlight/intel_backlight/brightness
+                        end
         end
 end

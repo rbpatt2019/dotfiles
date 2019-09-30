94426786d43fb7f1c51b05b1ef2e631c7f34cc13
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-09-30 18:33:11 -0400

    Update bright function

--- fish/functions/bright.fish
+++ fish/functions/bright.fish
@@ -2,19 +2,19 @@ function bright -a direction bright -d "Changes brightness on Intel backlight sy
         set -l max (cat /sys/class/backlight/intel_backlight/max_brightness)
         set -l min (math "0.05*$max")
         set -l step (math "$bright*$max")
-        set -l current (cat /sys/class/backlight/intel_backlight/actual_brightness)
+        set -l current (cat /sys/class/backlight/intel_backlight/brightness)
         switch $direction
                 case 'up'
                         if test (math "$step+$current") -gt $max
-                                echo $max | tee /sys/class/backlight/intel_backlight/brightness
+                                echo $max | sponge /sys/class/backlight/intel_backlight/brightness
                         else
-                                echo (math "$step+$current") | tee /sys/class/backlight/intel_backlight/brightness
+                                echo (math "$step+$current") | sponge /sys/class/backlight/intel_backlight/brightness
                         end
                 case 'down'
                         if test (math "$current-$step") -le $min
-                                echo $min | tee /sys/class/backlight/intel_backlight/brightness
+                                echo $min | sponge /sys/class/backlight/intel_backlight/brightness
                         else
-                                echo (math "$current-$step") | tee /sys/class/backlight/intel_backlight/brightness
+                                echo (math "$current-$step") | sponge /sys/class/backlight/intel_backlight/brightness
                         end
         end
 end

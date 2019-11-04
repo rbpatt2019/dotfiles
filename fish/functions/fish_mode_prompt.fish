a0bb7e286d6854bead5b4837eb1327b5ae5a26ca
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-11-04 14:44:12 +0000

    Configured vim mode settings

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,4 +1,20 @@
 function fish_mode_prompt
-	# Overriden by Spacefish fishshell theme
-	# To see vi mode in prompt add 'vi_mode' to SPACEFISH_PROMPT_ORDER
+        switch $fish_bind_mode
+                case default
+                        set_color white
+                        echo '[N]'
+                case insert
+                        set_color blue
+                        echo '[I]'
+                case replace_one
+                        set_color green
+                        echo '[R]'
+                case visual
+                        set_color purple
+                        echo '[V]'
+                case '*'
+                        set_color red
+                        echo '[?]'
+        end
+        set_color normal
 end

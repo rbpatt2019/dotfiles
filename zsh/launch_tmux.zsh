dd43ba307074acaae2f7b0737e77ba5a93b24d87
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-03-16 16:53:31 +0000

    Update Submodules

--- zsh/launch_tmux.zsh
+++ zsh/launch_tmux.zsh
@@ -1,22 +1,56 @@
 #!/usr/bin/zsh
 
-if [[ -z $TMUX ]] && [[ ${UID} -ne 0 ]]
+# Check necessary commands are installed...
+if ! hash tmux
 then
-        read "NEW_SESSION?Start a new TMUX session? (Leave blank to skip) "
-        tmux list-session
-        if [[ $? -eq 0 ]] && [[ -z $NEW_SESSION ]]
+        echo >&2 "Tmux not installed"
+        exit 1
+fi
+
+if ! hash tmuxinator
+then
+        echo >&2 "Tmuxinator not installed"
+        exit 1
+fi
+
+start_work () {
+        read "WORK?Start work? (y/n) "
+        if [[ "$WORK" = 'y' ]]
         then
-                echo ""
-                read "name?Attach to session: "
-                tmux attach-session -t $name > /dev/null 2>&1
-                while [[ $? -ne 0 ]]
-                do
-                        echo "Not a valid sessison!"
-                        echo ""
-                        read "name?Attach to session: "
-                        tmux attach-session -t $name
-                done
+                # If work is already running, then tmuxinator will attach, not relaunch
+                # So do not need to check for existence
+                tmuxinator work
         else
                 tmux new-session
+        fi
+}
+
+attach_to () {
+        NAME=${1:-"0"}
+        while ! tmux attach-session -t "$NAME" &> /dev/null
+        do
+                echo "Not a valid sessison!"
+                read "NAME?Attach to session: "
+        done
+}
+
+if [[ -z "$TMUX" ]] && [[ $UID -ne 0 ]]
+then
+        if tmux list-session 2> /dev/null
+        then
+                # tmux ls succeeds if a session exists...
+                echo "Type a session name to attach or 'new' for a new session."
+                read "SESSION?"
+                if [[ "$SESSION" = 'new' ]]
+                then 
+                        # Check whether to start work
+                        start_work 
+                else
+                        # Or attach to an existing
+                        attach_to "$SESSION"
+                fi
+        else
+                # ...and errors if one does not
+                start_work
         fi
 fi
